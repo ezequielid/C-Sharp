44eca0e272568503f3783011adff310fdb5baf39
unknown <ezequieltorres41@gmail.com>
2023-08-25 10:48:11 -0300

    subida pro1..

---
+++ pro1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Hacer un programa para ingresar un número y luego se emita por pantalla un cartel aclaratorio
+            //si “Es mayor a 10” o “No es mayor a 10”.
+            int n1;
+            Console.WriteLine("Ingrese un Número: ");
+            n1 = int.Parse(Console.ReadLine());
+            if(n1 >= 10){
+                Console.WriteLine("Es mayor a 10");
+            }else{
+                Console.WriteLine("No es mayor a 10");
+            }
+
+        }
+    }
+}
